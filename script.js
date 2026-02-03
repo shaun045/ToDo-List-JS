71d8b37f1ba4453ebe0614411e2461c05e42466d
shaun045 <shaun1adam2@gmail.com>
2026-02-03 21:03:53 +0800

    Add function, Save to LS, display Task

---
+++ script.js
@@ -0,0 +1,58 @@
+const btnAdd = document.getElementById('btn-add');
+const inp = document.getElementById('inp');
+const taskList = document.querySelector('.task-list');
+
+// Display Task in the list
+const displayData = () => {
+  const getTask = JSON.parse(localStorage.getItem("toDoTask")) ?? [];
+  let html = "";
+
+  getTask.map(item =>{
+    html += `<li>
+              <p>${item.data}</p>
+              <div class="btns">
+                  <i class="fa-solid fa-pen-to-square"></i>
+                  <i onClick="deleteItem(${item.id})" class="fa-solid fa-trash"></i>
+              </div>
+            </li>`
+  })
+
+  taskList.innerHTML = html;
+}
+
+displayData();
+
+//Delete task in the list
+const deleteItem = (id) => {
+  const getTask = JSON.parse(localStorage.getItem("toDoTask")) ?? [];
+  let newData = getTask.filter(item => item.id != id);
+}
+
+// Saving task to local Storage
+const saveTask = (task) => {
+  const getTask = JSON.parse(localStorage.getItem("toDoTask")) ?? [];
+
+  getTask.push({
+    id: Date.now(),
+    data: task
+  })
+
+  localStorage.setItem("toDoTask", JSON.stringify(getTask));
+  inp.value = "";
+  displayData()
+}
+
+
+
+
+// Button function for adding task
+btnAdd.addEventListener("click", () => {
+  let inpValue = inp.value.trim();
+
+  if(!inpValue) {
+    return alert("Input is Empty!")
+  }
+
+  saveTask(inpValue);
+
+})
