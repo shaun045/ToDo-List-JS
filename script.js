974e268485acc87742bfb75ce0cb0f51d3317586
shaun045 <shaun1adam2@gmail.com>
2026-02-04 03:46:10 +0800

    JS delete and update task

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const displayData = () => {
     html += `<li>
               <p>${item.data}</p>
               <div class="btns">
-                  <i class="fa-solid fa-pen-to-square"></i>
+                  <i onClick="updateItem(${item.id}, '${item.data}')" class="fa-solid fa-pen-to-square"></i>
                   <i onClick="deleteItem(${item.id})" class="fa-solid fa-trash"></i>
               </div>
             </li>`
@@ -22,10 +22,27 @@ const displayData = () => {
 
 displayData();
 
+// Update Item in the List
+const updateItem = (id, task)=> {
+  const editTask = prompt("Edit Your Task", task);
+  const getTask = JSON.parse(localStorage.getItem("toDoTask")) ?? [];
+
+  getTask.map(item => {
+    if(item.id === id) {
+      item.data = editTask;
+    }
+  })
+
+  localStorage.setItem("toDoTask", JSON.stringify(getTask));
+  displayData();
+}
+
 //Delete task in the list
 const deleteItem = (id) => {
   const getTask = JSON.parse(localStorage.getItem("toDoTask")) ?? [];
   let newData = getTask.filter(item => item.id != id);
+  localStorage.setItem("toDoTask", JSON.stringify(newData));
+  displayData();
 }
 
 // Saving task to local Storage
